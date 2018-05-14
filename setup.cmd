757ed7e44c33a5223e599cf7be46fb80bad69340
Jim Rubinsson <jim.rubinsson@episerver.com>
2018-05-14 08:37:17 +0200

    Remove database in app_data folder before copying the new one

--- setup.cmd
+++ setup.cmd
@@ -1,5 +1,14 @@
 @ECHO OFF
 
+SET AlloyMVC=src\Alloy.Mvc.Template
+
+IF EXIST %AlloyMVC%\App_Data (
+    ECHO Remove all files from the app data folder
+    DEL %AlloyMVC%\App_Data\*.* /F /Q || Exit /B 1
+) ELSE (
+    MKDIR %AlloyMVC%\App_Data || Exit /B 1
+)
+
 REM Copy the database files to the site.
 ROBOCOPY build/database/ src/Alloy.Mvc.Template/App_Data/ Alloy.mdf DefaultSiteContent.episerverdata /XC /XN /XO
 
